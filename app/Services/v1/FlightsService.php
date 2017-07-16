bf82451eaea7d28a2fafccfac5d6116a388ff82e
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-16 11:35:44 +0700

    filter->flight->index->by service

--- app/Services/v1/FlightsService.php
+++ app/Services/v1/FlightsService.php
@@ -8,6 +8,31 @@ class FlightsService
 {
 	public function getFlights()
 	{
-		return Flight::all();
+		// return Flight::all();
+		return $this->filterFlights(Flight::all());
 	}
+
+	//filtering flight
+	protected function filterFlights($flights)
+	{
+		$data = [];
+
+		foreach ($flights as $flight) {
+			$entry = [
+				'flightNumber' => $flight->flightNumber,
+				'status' => $flight->status,
+				'href' => route('flights.show',['id'=>$flight->flightNumber])
+			];
+
+			$data[] = $entry;
+		}
+
+		return $data;
+	}
+	//hasil
+	// {
+    //     "flightNumber": "1yn69572",
+    //     "status": "delayed",
+    //     "href": "http://localhost/laravel/latihan/API/airview_api/public/api/v1/flights/1yn69572"
+    // }
 }
